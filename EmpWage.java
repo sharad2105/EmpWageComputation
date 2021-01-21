485da1cb7cfc998c91ef0b58ddc19062d1b0bf43
unknown <patilsharad2000@gmail.com>
2021-01-21 14:21:14 +0530

    Check Employee Attendance Present or Absent

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,15 @@
-
 public class EmpWage {
 	   public static void main(String args[]) {
 	      System.out.println("Welcome to Employee With wage Computaion");
+
+				int IS_FULL_TIME=1;
+				double empCheck = Math.floor(Math.random() * 10) %2;
+				if (empCheck == IS_FULL_TIME) {
+					System.out.println("Employee is Present");
+				}
+				else {
+					System.out.println("Employee is Absent");
+				}
 	      }
 
 }
