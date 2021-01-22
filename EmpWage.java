cccf599d1d7cd962dbac1805c3f6bb219bcd2b71
unknown <patilsharad2000@gmail.com>
2021-01-22 21:54:57 +0530

    Using Clas Method To calculate Total Employee Wage

--- EmpWage.java
+++ EmpWage.java
@@ -3,9 +3,9 @@ public class EmpWage {
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAY = 20;
-
 	public static final int MAX_HRS_IN_MONTH = 100;
-	public static void main(String args[]) {
+
+public static int ComputeEmpWage() {
 
 		System.out.println("Welcome to Employee With wage Computaion");
 				//Veriables
@@ -35,5 +35,10 @@ public class EmpWage {
 	   }
 		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("total Emp Wage" +totalEmpWage);
-	}
+		return totalEmpWage;
+		}
+		public static void main(String args[]) {
+			ComputeEmpWage();
 
+	}
+}
